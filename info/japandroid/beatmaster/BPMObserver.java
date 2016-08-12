bf3cc57ff2cee56408fc3c9fe806eea088eddb98
Sven Sorrell <sven.sorrell@gmail.com>
2016-08-12 09:55:45 +0900

    first commit

---
+++ info/japandroid/beatmaster/BPMObserver.java
@@ -0,0 +1,5 @@
+package info.japandroid.beatmaster;
+
+public interface BPMObserver {
+	void updateBPM();
+}
---
+++ info/japandroid/beatmaster/DJTestDrive.java
@@ -0,0 +1,8 @@
+package info.japandroid.beatmaster;
+
+public class DJTestDrive{
+	public static void main(String[] args) {
+		BeatModelInterface model = new BeatModel();
+		ControllerInterface controller = new BeatController(model);
+	}	
+}
